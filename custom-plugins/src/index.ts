da797575fd7ecfefded8c126afcfc7146f58e0d1
Damian Tarnawsky <damian@ionic.io>
2022-07-12 09:45:41 -0700

    Initial Commit

--- custom-plugins/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-import { CameraPreviewPlugin } from './definitions';
-
-const CameraPreview = registerPlugin<CameraPreviewPlugin>('CameraPreview', {
-  web: () => import('./web').then((m) => new m.CameraPreviewWeb()),
-});
-
-export * from './definitions';
-export { CameraPreview };
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from 'custom-plugins/src';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+
+  async capture() {
+    BarcodeScanner.hideBackground(); // make background of WebView transparent
+    const result = await BarcodeScanner.startScan(); // start scanning and wait for a result
+    // if the result has content
+    if (result.hasContent) {
+      console.log(result.content); // log the raw scanned content
+    }
+  }
 
 }
